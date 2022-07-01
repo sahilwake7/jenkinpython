a5ec1d52de68af7694f247f818f57ef44163e0e3
Sahil Wake <96413042+sahilwake7@users.noreply.github.com>
2022-07-01 14:23:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
          
-       int num1 = 15, num2 = 15, sum;
+       int num1 = 25, num2 = 15, sum;
        sum = num1 + num2;
  
        System.out.println("Sum of these numbers: "+sum);
